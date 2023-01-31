9e6464b11e3ef8ca72dbe14416802c1ca6ce4d9c
liballejos <liballejos@ada.local.isima.fr>
2023-01-31 11:06:07 +0100

    nettoyage

--- code/Douanier.cpp
+++ code/Douanier.cpp
@@ -1,9 +1,28 @@
 #include <iostream>  // Inclusion d'un fichier standard
 #include "Douanier.hpp" // Inclusion d'un fichier du répertoire courant
 
+//Constructeur
 
-Douanier::Douanier(){
+Douanier::Douanier(std::string nom, std::string prenom) : Personne(nom, prenom){}
+Douanier::Douanier(){}
 
-    
-    
+// Fonction
+
+void Douanier::getVoisin(int &nombreVoisin, Position &voisin){
+    //tableau voisin de taille 8 = taille max des voisins possibles
+    int curseur = 0;
+    Position posDounier = getPose();
+    for(int i = ; i < 9; i++){
+        
+    }
+
+    nombreVoisin = curseur;
+}
+
+bool Douanier::getEnControle(){
+    return enControle;
+}
+
+void Douanier::setEnControle(bool valControle){
+    enControle = valControle;
 }
--- code/Douanier.hpp
+++ code/Douanier.hpp
@@ -2,11 +2,23 @@
 #define DOU_GUARD
 
 #include "Personne.hpp"
-class Douanier : public Personne {
+#include "Position.hpp"
 
+class Douanier : public Personne
+{
+        bool enControle;
+
+public:
         Douanier();
+        Douanier(std::string, std::string);
+        
 
+public:
+        void getVoisin(int &, Position&);
+        bool getEnControle();
+        void setEnControle(bool);
 };
 
+const double probaControle[8] = {0.6, 0.2, 0.1, 0.05, 0.05};
 
 #endif
