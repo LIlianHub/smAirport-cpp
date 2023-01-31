00df52645b2a1fe3cfcc39f0eea15a7b485fb6c0
liballejos <liballejos@ada.local.isima.fr>
2023-01-31 11:50:59 +0100

    douanieroooooooo

--- code/Douanier.cpp
+++ code/Douanier.cpp
@@ -1,28 +1,43 @@
-#include <iostream>  // Inclusion d'un fichier standard
+#include <iostream>     // Inclusion d'un fichier standard
 #include "Douanier.hpp" // Inclusion d'un fichier du répertoire courant
 
-//Constructeur
+// Constructeur
 
-Douanier::Douanier(std::string nom, std::string prenom) : Personne(nom, prenom){}
-Douanier::Douanier(){}
+Douanier::Douanier(std::string nom, std::string prenom) : Personne(nom, prenom), enControle{false}
+{
+}
+Douanier::Douanier() {}
 
 // Fonction
 
-void Douanier::getVoisin(int &nombreVoisin, Position &voisin){
-    //tableau voisin de taille 8 = taille max des voisins possibles
+void Douanier::getVoisin(int &nombreVoisin, Position voisin[])
+{
+    // tableau voisin de taille 8 = taille max des voisins possibles
     int curseur = 0;
     Position posDounier = getPos();
-    for(int i = 0; i < 9; i++){
-        
-    }
+    int info;
 
+    for (int i = posDounier.getY() - 1; i <= posDounier.getY() + 1; i++)
+    {
+        for (int j = posDounier.getX(); j <= posDounier.getX() + 1; j++)
+        {
+            info = p.OnthisCase(i, j);
+            if (info == 1 && i != posDounier.getY() && j != posDounier.getX())
+            {
+                voisin[curseur] = Position(j, i);
+                curseur++;
+            }
+        }
+    }
     nombreVoisin = curseur;
 }
 
-bool Douanier::getEnControle(){
+bool Douanier::getEnControle()
+{
     return enControle;
 }
 
-void Douanier::setEnControle(bool valControle){
+void Douanier::setEnControle(bool valControle)
+{
     enControle = valControle;
 }
--- code/Douanier.hpp
+++ code/Douanier.hpp
@@ -3,6 +3,7 @@
 
 #include "Personne.hpp"
 #include "Position.hpp"
+#include "Map.hpp"
 
 class Douanier : public Personne
 {
@@ -14,11 +15,11 @@ public:
         
 
 public:
-        void getVoisin(int &, Position&);
+        void getVoisin(int &, Position[]);
         bool getEnControle();
         void setEnControle(bool);
 };
 
 const double probaControle[8] = {0.6, 0.2, 0.1, 0.05, 0.05};
-
+extern Map p;
 #endif
