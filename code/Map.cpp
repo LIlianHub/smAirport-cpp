5765a01182ab177c8b072d00b7c458bab1012d91
Etienne Charpin <etiennecharpin@gmail.com>
2023-01-31 11:06:29 +0100

    dd

--- code/Map.cpp
+++ code/Map.cpp
@@ -8,7 +8,37 @@ Map::Map(){
 void Map::AfficherMap(){
     for(int i = 0; i < 20 ; i++){
         for(int j = 0; j < 20 ; j++){
-            std::cout << " . ";
+
+            switch (grille[i][j])
+            {
+            case 0:
+                std::cout << " . ";
+                break;
+
+            case 1:
+                std::cout << " X ";
+                break;
+
+            case 2:
+                std::cout << " D ";
+                break;
+
+            case 3:
+                std::cout << " B ";
+                break;
+
+            case 4:
+                std::cout << " L ";
+                break;
+
+            case 5:
+                std::cout << " Z ";
+                break;
+            
+            default:
+                break;
+            }
+            
         } 
         std::cout << std::endl;
     }
