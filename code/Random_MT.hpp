4a152acf301d669566c77df2f81c4f0b2f78e188
hudenizot <hugo.denizot@etu.uca.fr>
2023-01-31 11:25:57 +0100

    Ajout MT

--- code/Random_MT.hpp
+++ code/Random_MT.hpp
@@ -1,8 +1,19 @@
 #ifndef RANDOM_MT_GUARD
 #define RANDOM_MT_GUARD
 
-static class Random_MT{
-    
-}
+class Random_MT{
+
+
+    public:
+        static void init_genrand(unsigned long s);
+        static void init_by_array(unsigned long init_key[], int key_length);
+        
+        static unsigned long genrand_int32(void);
+        static long genrand_int31(void);
+        static double genrand_real1(void);
+        static double genrand_real2(void);
+        static double genrand_real3(void);
+        static double genrand_res53(void);
+};
 
 #endif
