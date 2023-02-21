ab9d1cb750e2e226d2704a1b7ebc6e42a8582155
Etienne Charpin <etiennecharpin@gmail.com>
2023-02-21 11:54:07 +0100

    ee

--- code/main.cpp
+++ code/main.cpp
@@ -15,22 +15,28 @@ Map m{};
 int main(int, char **) {
 
   Visiteur pers(Position(0,0));
-  Douanier persd(Position(2,0));
-  m.tabpers[m.nbpersonne] = & pers;
-  m.nbpersonne++;
-  m.tabpers[m.nbpersonne] = & persd;
-  m.nbpersonne++;
+  Douanier persd(Position(1,0));
+  m.ajouterPers(&pers);
+  m.ajouterPers(&persd);
+  m.grille[1][0] = 1;
+  m.grille[0][0] = 2;
   m.grille[12][13] = 3;
-  m.grille[pers.getPos().getX()][pers.getPos().getY()] = 1;
   int temps = 0;
+
+  int test;
+  Position info[9];
+
 
   while (temps < 20)
   {
+    persd.getVoisin(test, info);
+    std::cout << test << std::endl;
     for(int i=0;i<m.nbpersonne;i++){
-      m.tabpers[i]->Action();
+      //m.tabpers[i]->deplacerPersonne(Position(0,temps));
+      //m.tabpers[i]->Action();
     }
     //system("clear");
-    //m.AfficherMap();
+    m.AfficherMap();
     std::cout << temps << std::endl;
     temps++;
     sleep(1);
